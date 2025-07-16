20d632951a29f4d813fff10a6507f68286b6d2d3
summer <gupy1998@gmail.com>
2025-07-16 19:45:37 +0900

    fix mistral api key

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.REACT_APP_MIR_API_KEY}`,
+        "x-api-key": process.env.REACT_APP_MIR_API_KEY!,
       },
       body: JSON.stringify({ prompt: "Hello" }),
     })
