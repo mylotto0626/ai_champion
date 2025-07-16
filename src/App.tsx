296ccbb5d121cd11577a1701ca70a89ec5354955
summer <gupy1998@gmail.com>
2025-07-16 18:46:32 +0900

    fix env and api key 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import CheckupAnalysis from "./pages/CheckupAnalysis";
 import NotFound from "./pages/NotFound";
 import { useEffect } from "react";
 const queryClient = new QueryClient();
-// api key 추가
+// api key 추가 env 파일에 추가
 
 const App = () => {
   useEffect(() => {
